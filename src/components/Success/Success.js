8ed9e92a15c59a00c28241f77950902722e5c5c4
Felix A. Schultz <felix.schultz@engapho.com>
2022-09-10 17:22:11 +0200

    added more style & order information

--- src/components/Success/Success.js
+++ src/components/Success/Success.js
@@ -1,9 +1,18 @@
 const { useState } = React;
 import "./Style/Success.css";
 export default function SuccessWindow(props) {
+    
+    function formatDate(date) {
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        hours = hours;
+        minutes = minutes < 10 ? '0'+minutes : minutes;
+        var strTime = hours + ':' + minutes;
+        return (date.getDay()) + "." + (date.getMonth()+1) + " " + date.getFullYear() + "  " + strTime;
+    }
+
     const [popup, setPopUp] = useState(false);
     function makePurchase(order) {
-        console.log(order);
         setPopUp(!popup);
     }
 
@@ -15,13 +24,15 @@ export default function SuccessWindow(props) {
                     <h2>Tilgængelige ruter:</h2>
                     <p>From: { props.values.fromharbor }</p>
                     <p>To: {props.values.toharbor}</p>
+                    <p>Antal Personer: {props.values.passangerCount}</p>
+                    <p>Dato: { formatDate(new Date(props.values.dep)) }</p>
                     {(!!+props.values.cycle) ? <p>Inkl. cykel</p> : null}
-                    <button onClick={ () =>  makePurchase(props.order) }>Køb ticket</button>
+                    <button className="cta" onClick={ () =>  makePurchase(props.order) }>Køb ticket</button>
                 </section>
             </article>
             {(popup) ? <article className="successWindow">
                 <section className="successWindow__content">
-                    <button onClick={ () =>  setPopUp(!popup) }>Close</button>
+                    <button className="" onClick={ () =>  setPopUp(!popup) }>Close</button>
                 </section>
             </article> : null }
         </>
