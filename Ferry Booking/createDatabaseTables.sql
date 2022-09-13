ea6a1149c762f452be393bf7d089fd566820ec35
Felix A. Schultz <felix.schultz@engapho.com>
2022-09-13 15:29:30 +0200

    Changed SQL

--- Ferry Booking/createDatabaseTables.sql
+++ Ferry Booking/createDatabaseTables.sql
@@ -1,39 +1,39 @@
 CREATE TABLE ships(
     id INT AUTO_INCREMENT,
     passengers INT,
-    `name` VARCHAR(MAX),
+    `name` VARCHAR,
     bicycles INT
 ); CREATE TABLE routes(
     id INT AUTO_INCREMENT,
-    `name` VARCHAR(MAX)
+    `name` VARCHAR
 ); CREATE TABLE stops(
     id INT AUTO_INCREMENT,
-    `name` VARCHAR(MAX)
+    `name` VARCHAR
 ); CREATE TABLE stops_per_route(
     id INT NOT NULL AUTO_INCREMENT,
     stop_id INT,
     route_id INT
 ); CREATE TABLE customers(
     id INT AUTO_INCREMENT,
-    `name` VARCHAR(MAX),
-    email VARCHAR(MAX),
-    phone VARCHAR(MAX),
-    street VARCHAR(MAX),
-    street2 VARCHAR(MAX),
-    zip VARCHAR(MAX),
-    city VARCHAR(MAX),
-    county VARCHAR(MAX),
-    country VARCHAR(MAX)
+    `name` VARCHAR,
+    email VARCHAR,
+    phone VARCHAR,
+    street VARCHAR,
+    street2 VARCHAR,
+    zip VARCHAR,
+    city VARCHAR,
+    county VARCHAR,
+    country VARCHAR
 ); CREATE TABLE customer_types(
     id INT AUTO_INCREMENT,
-    `name` VARCHAR(MAX)
+    `name` VARCHAR
 ); CREATE TABLE prices_per_route(
     route_id INT,
     customer_type INT,
     base_price DECIMAL(6, 3)
 ); CREATE TABLE coupons(
     id INT AUTO_INCREMENT,
-    `code` VARCHAR(MAX),
+    `code` VARCHAR,
     active BOOLEAN,
     `type` ENUM('fixed', 'percentage'),
     `value` DECIMAL(6, 3)
@@ -51,7 +51,7 @@ CREATE TABLE ships(
     booking_id INT,
     timetable_id INT,
     seat_type ENUM("passenger", "bicycle"),
-    seat_name VARCHAR(MAX),
+    seat_name VARCHAR,
     seat_value INT
 ); CREATE TABLE names_per_language(
     `name` VARCHAR(MAX),
@@ -60,7 +60,7 @@ CREATE TABLE ships(
 ); CREATE TABLE timetables(
     id INT AUTO_INCREMENT,
     stops_per_route_id INT,
-    `weekday` VARCHAR(MAX),
+    `weekday` VARCHAR,
     times TIME,
     active_from TIMESTAMP,
     active_to TIMESTAMP
